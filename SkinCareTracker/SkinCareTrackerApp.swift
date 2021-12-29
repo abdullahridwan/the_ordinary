5b74c77e94733b135860f63cbfb2bd069eab9728
Abdullah Ridwan <abdullahridwan73@icloud.com>
2021-12-29 11:33:40 -0500

    Initial Commit

---
+++ SkinCareTracker/SkinCareTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkinCareTrackerApp.swift
+//  SkinCareTracker
+//
+//  Created by Abdullah Ridwan on 12/29/21.
+//
+
+import SwiftUI
+
+@main
+struct SkinCareTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
